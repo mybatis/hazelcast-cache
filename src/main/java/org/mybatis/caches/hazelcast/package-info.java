f6077b4816576e1f4a8e787f29f83079ec52e041
Simone Tripodi <simone.tripodi@gmail.com>
2010-08-09 06:21:33 +0000

    fixed headers

--- src/main/java/org/mybatis/caches/hazelcast/package-info.java
+++ src/main/java/org/mybatis/caches/hazelcast/package-info.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright 2009-2010 The iBaGuice Team
+ *    Copyright 2010 The MyBatis Team
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  */
 
 /**
- * Contains Hazelcast support for iBatis Cache.
+ * Contains Hazelcast support for MyBatis Cache.
  *
  * @version $Id$
  */
