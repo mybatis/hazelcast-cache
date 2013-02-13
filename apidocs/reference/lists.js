edcaa0fbb84528911d5fde02d13b815f68b07dbb
Simone Tripodi <simonetripodi@apache.org>
2013-02-13 11:26:21 -0800

    Create gh-pages branch via GitHub

--- apidocs/reference/lists.js
+++
@@ -1,6 +0,0 @@
-var DATA = [
-      { id:0, label:"org.mybatis.caches.hazelcast", link:"org/mybatis/caches/hazelcast/package-summary.html", type:"package" },
-      { id:1, label:"org.mybatis.caches.hazelcast.HazelcastCache", link:"org/mybatis/caches/hazelcast/HazelcastCache.html", type:"class" },
-      { id:2, label:"org.mybatis.caches.hazelcast.LoggingHazelcastCache", link:"org/mybatis/caches/hazelcast/LoggingHazelcastCache.html", type:"class" }
-
-    ];
