0beb56547162e17c18083d4bdfb1aabb8e6df3ce
Simo Tripodi <stripodi@adobe.com>
2013-02-15 17:46:20 +0100

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,7 @@
+var NAVTREE_DATA =
+[ [ "org.mybatis.caches.hazelcast", "org/mybatis/caches/hazelcast/package-summary.html", [ [ "Description", "org/mybatis/caches/hazelcast/package-descr.html", null, "" ], [ "Classes", null, [ [ "HazelcastCache", "org/mybatis/caches/hazelcast/HazelcastCache.html", null, "" ], [ "LoggingHazelcastCache", "org/mybatis/caches/hazelcast/LoggingHazelcastCache.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
