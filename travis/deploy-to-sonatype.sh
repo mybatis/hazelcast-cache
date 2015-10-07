7af276cbda241680d8149e4d7e0496d6717992fa
Iwao AVE! <harawata@gmail.com>
2015-10-07 15:13:31 +0900

    With the previous commit, sonatype returned error 401 Unauthorized. Added -X switch for debugging.

--- travis/deploy-to-sonatype.sh
+++ travis/deploy-to-sonatype.sh
@@ -1,5 +1,5 @@
 mybatis_repo=$(git config --get remote.origin.url 2>&1)
 if [ "$mybatis_repo" == "https://github.com/mybatis/hazelcast-cache.git" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  mvn deploy --settings ./travis/settings.xml
+  mvn -X deploy --settings ./travis/settings.xml
   echo -e "Successfully deployed SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
 fi
