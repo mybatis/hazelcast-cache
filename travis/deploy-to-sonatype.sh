2294c7783dfa165523f205568f4edfc5323baf9a
Jeremy Landis <jeremylandis@hotmail.com>
2015-10-07 21:28:53 -0400

    [travis] Rename travis script to after_success, add coveralls, and run on java 6 (quietly)

--- travis/deploy-to-sonatype.sh
+++
@@ -1,5 +0,0 @@
-mybatis_repo=$(git config --get remote.origin.url 2>&1)
-if [ "$mybatis_repo" == "https://github.com/mybatis/hazelcast-cache.git" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-  mvn deploy --settings ./travis/settings.xml
-  echo -e "Successfully deployed SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
-fi
